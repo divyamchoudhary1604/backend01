86557f0d44240e273f33fdd48f59d9b45598cb5c
Divyam Kumar <divyamchoudhary1604@gmail.com>
2026-09-16 02:29:32 +0530

    routes connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,18 @@
+import dns from "node:dns";
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";   //double dot to go back one folder
+import { DB_NAME } from "../constants.js";
 
+// Use public DNS to resolve MongoDB Atlas SRV records reliably
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const connect_DB = async ()=>{                 //IIFE
+const connect_DB = async () => {
     try {
-        const connectionInstance = await mongoose.connect
-        (`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST:
-            ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGO_DB connection FAILED",error);
+        console.log("MONGO_DB connection FAILED: ", error);
+        process.exit(1);
     }
-}
+};
 
-export default connect_DB                                                                              
-
+export default connect_DB;
